2c65dfc3475074627b21dfa5e7e373369b386eb8
JCPrime01 <vanzokiller@gmail.com>
2026-06-06 09:39:55 -0300

    chore: shareCode ROI20RQZBHH -> 2S86VU20M91 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=ROI20RQZBHH&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=2S86VU20M91&afp=oa' });
     res.end();
   }
