436f85c2db3c3407724fa2ef02bf3d333c08174b
JCPrime01 <vanzokiller@gmail.com>
2026-06-07 09:46:59 -0300

    chore: shareCode 2S86VU20M91 -> 3XCU9UL2Q78 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=2S86VU20M91&afp=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=3XCU9UL2Q78&afp=oa' });
     res.end();
   }
