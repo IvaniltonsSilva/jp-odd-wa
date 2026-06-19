0b5f87af165ca2eb98dbf1b402613cc20d81b7b8
JCPrime01 <vanzokiller@gmail.com>
2026-06-19 12:28:21 -0300

    chore: shareCode ME54PV6TA7B -> X8XX9M557UI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=ME54PV6TA7B&afp1=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=X8XX9M557UI&afp1=oa' });
     res.end();
   }
