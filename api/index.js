c8fd41848158c12a75b0bdf3bf484480a07551b0
JCPrime01 <vanzokiller@gmail.com>
2026-06-28 01:53:00 -0300

    chore: shareCode O6J32RHEAV8 -> 8B7IQTMQGF5 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=O6J32RHEAV8&afp1=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=8B7IQTMQGF5&afp1=oa' });
     res.end();
   }
