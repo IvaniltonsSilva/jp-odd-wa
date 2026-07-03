230dc4f094723790e24c6b83b63b1be947a3bc8a
JCPrime01 <vanzokiller@gmail.com>
2026-07-03 10:36:33 -0300

    chore: shareCode 2XPJBM4QYLV -> N20BZUK9OBF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=2XPJBM4QYLV&afp1=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=N20BZUK9OBF&afp1=oa' });
     res.end();
   }
