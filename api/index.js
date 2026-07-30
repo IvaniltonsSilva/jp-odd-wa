919e23cfbed0dc4e817a32510bc48a8a459c4df2
JCPrime01 <vanzokiller@gmail.com>
2026-07-30 13:03:22 -0300

    chore: shareCode 0XVKE4U5HML -> Q90X6GYODCQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=0XVKE4U5HML&afp1=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7180&shareCode=Q90X6GYODCQ&afp1=oa' });
     res.end();
   }
